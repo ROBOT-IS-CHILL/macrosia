d0c5f3e519e49a8bae66826d61c0d82efa38f27f
balt-dev <heptor42@gmail.com>
2026-01-22 11:04:07 -0600

    Lazy execution

--- src/expr.rs
+++ src/expr.rs
@@ -98,6 +98,39 @@ pub struct ExpressionFunction {
 	arg_count: u32
 }
 
+#[derive(Clone)]
+enum StackEntry {
+	FuncCall {
+		name: Vec<u8>,
+		args: Vec<StackEntry>
+	},
+	Operation {
+		operator: Operator,
+		args: Vec<StackEntry>
+	},
+	Literal(Number)
+}
+impl StackEntry {
+	fn eval(self, var: &VariableRegistry, depth: usize, name: &[u8]) -> Result<Number, MacroError> {
+		const DEPTH_LIMIT: usize = 1024;
+		if depth > DEPTH_LIMIT {
+			return Err(format!("in {}: function call depth limit of {DEPTH_LIMIT} exceeded", String::from_utf8_lossy(name)))?;
+		}
+		match self {
+			Self::Operation { operator, mut args } => 
+				operator.eval(&mut args, var, depth, name).ok_or_else(|| format!("in {}: operator has not enough args past check, should never happen", String::from_utf8_lossy(name)).into()),
+			Self::FuncCall { name: child, args } => {
+				let func = var.load_fn(&child).ok_or_else(|| format!("in {}: function with name {} does not exist", String::from_utf8_lossy(name), String::from_utf8_lossy(&child)))?;
+				let mut stack = Vec::<StackEntry>::new();
+				func._exec(&mut stack, &args, var, &child)?;
+				stack.pop().ok_or_else(|| format!("in {}: stack was empty at end of function", String::from_utf8_lossy(name)))?
+					.eval(var, depth + 1, &child)
+			}
+			Self::Literal(num) => Ok(num)
+		}
+	}
+}
+
 impl ExpressionFunction {
 	pub fn parse(string: &[u8]) -> Result<Self, MacroError> {
 		let nodes = string.split(|b| b.is_ascii_whitespace())
@@ -120,33 +153,33 @@ impl ExpressionFunction {
 	#[inline]
 	pub(crate) fn exec(&self, args: &[Number], reg: &VariableRegistry, name: &[u8]) -> Result<Number, MacroError> {
 		let mut stack = Vec::new();
-		self._exec(&mut stack, args.iter().map(|v| *v).rev().collect::<Vec<_>>().as_slice(), reg, name, 0)?;
-		stack.pop().ok_or_else(|| format!("in {}: stack empty", String::from_utf8_lossy(name)).into())
+		self._exec(&mut stack, args.iter().map(|v| StackEntry::Literal(*v)).rev().collect::<Vec<_>>().as_slice(), reg, name)?;
+		let res = stack.pop().ok_or_else(|| format!("in {}: stack empty", String::from_utf8_lossy(name)))?;
+		res.eval(reg, 0, name)
 	}
 
-	fn _exec(&self, stack: &mut Vec<Number>, args: &[Number], reg: &VariableRegistry, name: &[u8], depth: u32) -> Result<(), MacroError> {
-		const DEPTH_LIMIT: u32 = 1024;
-		if depth > DEPTH_LIMIT {
-			return Err(format!("in {}: function call depth limit of {DEPTH_LIMIT} exceeded", String::from_utf8_lossy(name)))?;
-		}
+	fn _exec(&self, stack: &mut Vec<StackEntry>, args: &[StackEntry], reg: &VariableRegistry, name: &[u8]) -> Result<(), MacroError> {
 		if self.arg_count != args.len() as u32 {
 			return Err(format!("in {}: function takes {} arguments, {} given", String::from_utf8_lossy(name), self.arg_count, args.len()))?;
 		}
 		for node in &self.nodes {
 			match node {
 				Node::Operator(opr) => {
-					let res = opr.eval(stack).ok_or_else(|| format!("in {}: operator {opr:?} not given enough arguments (expected {})", String::from_utf8_lossy(name), opr.argument_count()))?;
-					stack.push(res);
+					if stack.len() < opr.argument_count() as usize {
+						return Err(format!("in {}: operator {opr:?} not given enough arguments (expected {})", String::from_utf8_lossy(name), opr.argument_count()))?;
+					}
+					let args = stack.split_off(stack.len() - (opr.argument_count() as usize));
+					stack.push(StackEntry::Operation { operator: *opr, args });
 				},
 				Node::Input(number) => {
 					if *number > self.arg_count {
 						return Err(format!("in {}: input node takes more than arg count (this should never happen)", String::from_utf8_lossy(name)))?;
 					}
 					let val = args.get((self.arg_count - number) as usize).ok_or_else(|| format!("in {}: function takes {} arguments, {} given", String::from_utf8_lossy(name), self.arg_count, args.len()))?;
-					stack.push(*val);
+					stack.push(val.clone());
 				},
 				Node::Number(num) => {
-					stack.push(*num);
+					stack.push(StackEntry::Literal(*num));
 				},
 				Node::FuncCall(fun) => {
 					let func = reg.load_fn(&fun).ok_or_else(|| format!("in {}: function with name {} does not exist", String::from_utf8_lossy(name), String::from_utf8_lossy(fun)))?;
@@ -154,7 +187,7 @@ impl ExpressionFunction {
 						return Err(format!("in {}: function {} takes {} arguments, {} given", String::from_utf8_lossy(name), String::from_utf8_lossy(fun), func.arg_count, stack.len()))?;
 					}
 					let args = stack.split_off(stack.len() - (func.arg_count as usize)).into_iter().rev().collect::<Vec<_>>();
-					func._exec(stack, &args, reg, fun, depth + 1)?;
+					stack.push(StackEntry::FuncCall { name: fun.to_vec(), args });
 				}
 			}
 		}
@@ -163,23 +196,26 @@ impl ExpressionFunction {
 }
 
 impl Operator {
-	fn eval(&self, stack: &mut Vec<Number>) -> Option<Number> {
+	fn eval(&self, stack: &mut Vec<StackEntry>, var: &VariableRegistry, depth: usize, name: &[u8]) -> Option<Number> {
 		use self::*;
+		macro_rules! spop {
+			() => { stack.pop()?.eval(var, depth + 1, name).ok()? }
+		}
 		Some(match self {
-			Self::Add => stack.pop()? + stack.pop()?,
-			Self::Sub => {let [b, a] = [stack.pop()?, stack.pop()?]; a - b},
-			Self::Mul => stack.pop()? * stack.pop()?,
-			Self::Div => {let [b, a] = [stack.pop()?, stack.pop()?]; a / b},
-			Self::Mod => {let [b, a] = [stack.pop()?, stack.pop()?]; a % b},
-			Self::Neg => -stack.pop()?,
-			Self::Less => { let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer((a < b) as i64) },
-			Self::Leq => { let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer((a <= b) as i64) },
-			Self::Great => { let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer((a > b) as i64) },
-			Self::Geq => { let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer((a >= b) as i64) },
-			Self::Eq => Number::Integer((stack.pop()? == stack.pop()?) as i64),
-			Self::Neq => Number::Integer((stack.pop()? != stack.pop()?) as i64),
+			Self::Add => spop!() + spop!(),
+			Self::Sub => {let [b, a] = [spop!(), spop!()]; a - b},
+			Self::Mul => spop!() * spop!(),
+			Self::Div => {let [b, a] = [spop!(), spop!()]; a / b},
+			Self::Mod => {let [b, a] = [spop!(), spop!()]; a % b},
+			Self::Neg => -spop!(),
+			Self::Less => { let [b, a] = [spop!(), spop!()]; Number::Integer((a < b) as i64) },
+			Self::Leq => { let [b, a] = [spop!(), spop!()]; Number::Integer((a <= b) as i64) },
+			Self::Great => { let [b, a] = [spop!(), spop!()]; Number::Integer((a > b) as i64) },
+			Self::Geq => { let [b, a] = [spop!(), spop!()]; Number::Integer((a >= b) as i64) },
+			Self::Eq => Number::Integer((spop!() == spop!()) as i64),
+			Self::Neq => Number::Integer((spop!() != spop!()) as i64),
 			Self::Cmp => {
-				let [b, a] = [stack.pop()?, stack.pop()?];
+				let [b, a] = [spop!(), spop!()];
 				Number::Float(
 					// Ordering::Less => -1, Ordering::Equal => 0, Ordering::Greater => 1
 					// Thanks, rust stdlib
@@ -187,63 +223,63 @@ impl Operator {
 				)
 			},
 			Self::Tern => {
-				let falsy = stack.pop()?;
-			    let truthy = stack.pop()?;
-			    let cond = stack.pop()?;
+				let falsy = spop!();
+			    let truthy = spop!();
+			    let cond = spop!();
 				if cond == Number::ZERO { falsy } else { truthy }
 			},
-			Self::And => Number::Integer(i64::from(stack.pop()?) & i64::from(stack.pop()?)),
-			Self::Or => Number::Integer(i64::from(stack.pop()?) | i64::from(stack.pop()?)),
-			Self::Xor => Number::Integer(i64::from(stack.pop()?) ^ i64::from(stack.pop()?)),
-			Self::Not => Number::Integer(!i64::from(stack.pop()?)),
-			Self::Shl => {let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer(i64::from(a) << ((i64::from(b) as u64) % 64))},
-			Self::Shr => {let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer(((i64::from(a) as u64) >> (i64::from(b) as u64 % 64)) as i64)},
-			Self::AShr => {let [b, a] = [stack.pop()?, stack.pop()?]; Number::Integer(i64::from(a) >> ((i64::from(b) as u64) % 64))},
-			Self::Pow => {let [b, a] = [stack.pop()?, stack.pop()?]; a.pow(b)},
-			Self::Log => {let [b, a] = [stack.pop()?, stack.pop()?]; a.log(b)},
-			Self::Abs => match stack.pop()? {
+			Self::And => Number::Integer(i64::from(spop!()) & i64::from(spop!())),
+			Self::Or => Number::Integer(i64::from(spop!()) | i64::from(spop!())),
+			Self::Xor => Number::Integer(i64::from(spop!()) ^ i64::from(spop!())),
+			Self::Not => Number::Integer(!i64::from(spop!())),
+			Self::Shl => {let [b, a] = [spop!(), spop!()]; Number::Integer(i64::from(a) << ((i64::from(b) as u64) % 64))},
+			Self::Shr => {let [b, a] = [spop!(), spop!()]; Number::Integer(((i64::from(a) as u64) >> (i64::from(b) as u64 % 64)) as i64)},
+			Self::AShr => {let [b, a] = [spop!(), spop!()]; Number::Integer(i64::from(a) >> ((i64::from(b) as u64) % 64))},
+			Self::Pow => {let [b, a] = [spop!(), spop!()]; a.pow(b)},
+			Self::Log => {let [b, a] = [spop!(), spop!()]; a.log(b)},
+			Self::Abs => match spop!() {
 				Number::Complex(c) => Number::Float(c.norm()),
 				other => if other < Number::ZERO {-other} else {other} 
 			},
-			Self::Sin => match stack.pop()? {
+			Self::Sin => match spop!() {
 				Number::Integer(i) => Number::Float((i as f64).sin()),
 				Number::Float(f) => Number::Float(f.sin()),
 				Number::Complex(c) => Number::Complex(c.sin()),
 			},
-			Self::Cos => match stack.pop()? {
+			Self::Cos => match spop!() {
 				Number::Integer(i) => Number::Float((i as f64).cos()),
 				Number::Float(f) => Number::Float(f.cos()),
 				Number::Complex(c) => Number::Complex(c.cos()),
 			},
-			Self::Tan => match stack.pop()? {
+			Self::Tan => match spop!() {
 				Number::Integer(i) => Number::Float((i as f64).tan()),
 				Number::Float(f) => Number::Float(f.tan()),
 				Number::Complex(c) => Number::Complex(c.tan()),
 			},
-			Self::Asin => match stack.pop()? {
+			Self::Asin => match spop!() {
 				Number::Integer(i) => Number::Float((i as f64).asin()),
 				Number::Float(f) => Number::Float(f.asin()),
 				Number::Complex(c) => Number::Complex(c.asin()),
 			},
-			Self::Acos => match stack.pop()? {
+			Self::Acos => match spop!() {
 				Number::Integer(i) => Number::Float((i as f64).acos()),
 				Number::Float(f) => Number::Float(f.acos()),
 				Number::Complex(c) => Number::Complex(c.acos()),
 			},
-			Self::Atan => match stack.pop()? {
+			Self::Atan => match spop!() {
 				Number::Integer(i) => Number::Float((i as f64).atan()),
 				Number::Float(f) => Number::Float(f.atan()),
 				Number::Complex(c) => Number::Complex(c.atan()),
 			},
-			Self::Real => match stack.pop()? {
+			Self::Real => match spop!() {
 				Number::Complex(c) => Number::Float(c.re),
 				other => other
 			},
-			Self::Imag => match stack.pop()? {
+			Self::Imag => match spop!() {
 				Number::Complex(c) => Number::Float(c.im),
 				other => Number::ZERO
 			},
-			Self::Arg => Number::Float(Complex64::from(stack.pop()?).arg()),
+			Self::Arg => Number::Float(Complex64::from(spop!()).arg()),
 		})
 	}
 }
